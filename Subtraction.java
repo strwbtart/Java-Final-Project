ba7f6f0971bf0f2eea15300057f99f27783b0501
strwbtart <emmapach05@gmail.com>
2025-08-13 12:12:41 -0500

    subtraction class

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+// This class also extends the Operation class, providing the logic for subtraction.
+//----------------------------------------------
+public class Subtraction extends Operation {
+    // This is our concrete implementation of 'perform' for subtraction.
+    @Override
+    public double perform(double num1, double num2) {
+        return num1 - num2;
+    }
+}
