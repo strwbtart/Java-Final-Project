f2cdf273b8416762b4fc563b4c9f1b98e523477f
strwbtart <emmapach05@gmail.com>
2025-08-13 12:13:27 -0500

    multiplication method

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+// Extends Operation for multiplication.
+//----------------------------------------------
+public class Multiplication extends Operation {
+    // Our 'perform' method for multiplication.
+    @Override
+    public double perform(double num1, double num2) {
+        return num1 * num2;
+    }
+}
