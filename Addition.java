06b159935769b1d40d2de747142a94b0dd3e9e46
CarolinaCG03 <carolina.cantugarcia01@utrgv.edu>
2025-08-13 12:21:53 -0500

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -4,7 +4,7 @@
 public class Addition extends Operation {
     // This method is a concrete implementation of the abstract 'perform' method.
     // It's a simple function that adds two numbers together.
-    @Override
+    
     public double perform(double num1, double num2) {
         return num1 + num2;
     }
