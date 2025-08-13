0990f28f75630fa751a059b03aea206ed00d7b79
CarolinaCG03 <carolina.cantugarcia01@utrgv.edu>
2025-08-13 12:21:13 -0500

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -2,7 +2,7 @@
 //----------------------------------------------
 public class Multiplication extends Operation {
     // Our 'perform' method for multiplication.
-    @Override
+    
     public double perform(double num1, double num2) {
         return num1 * num2;
     }
