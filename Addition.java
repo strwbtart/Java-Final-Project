6e3fa13280cf4d2f460797f500273770be97fe57
strwbtart <emmapach05@gmail.com>
2025-08-15 18:53:38 -0500

    addition comments

--- Addition.java
+++ Addition.java
@@ -1,10 +1,11 @@
-// This class "extends" (or inherits from) the Operation class.
-// It provides the specific logic for adding two numbers.
-//----------------------------------------------
+// This is the file for our Addition class.
+// See how it "extends" Operation? That means it's using the blueprint from our last file.
+// It's like we're filling out the form we just created.
+
 public class Addition extends Operation {
-    // This method is a concrete implementation of the abstract 'perform' method.
-    // It's a simple function that adds two numbers together.
-    
+    // This is our code for the "perform" method that we were told to create.
+    // We're literally just telling it to add the two numbers together.
+    @Override
     public double perform(double num1, double num2) {
         return num1 + num2;
     }
