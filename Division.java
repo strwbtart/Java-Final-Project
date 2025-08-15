c614f36462576641257e5e3fd972bc844114a83c
strwbtart <emmapach05@gmail.com>
2025-08-15 18:55:40 -0500

    division comments

--- Division.java
+++ Division.java
@@ -1,12 +1,17 @@
-// Extends Operation for division. Includes a check to prevent division by zero.
-//----------------------------------------------
+// Last one for our math operations! This is for division.
+// It also uses the Operation blueprint, but has a little extra smarts built in.
+
 public class Division extends Operation {
-    // Our 'perform' method for division.
+    // This is our "perform" method for division.
+    @Override
     public double perform(double num1, double num2) {
-        // Here we handle a potential error: division by zero.
+        // Here's the special part: we're checking if the second number is zero.
+        // You can't divide by zero, so we want to stop that from happening and not break our app.
         if (num2 == 0) {
             System.out.println("Error: Cannot divide by zero.");
-            return Double.NaN; // 'NaN' stands for Not a Number. It's a special value to show an error.
+            // We'll return a special value called "Double.NaN" which stands for "Not a Number".
+            // It's a way to signal that something went wrong with the math.
+            return Double.NaN; 
         }
         return num1 / num2;
     }
