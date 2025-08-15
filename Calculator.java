cd876756b739c4dd2cecb193fe76b326adf2c6e3
strwbtart <emmapach05@gmail.com>
2025-08-15 18:57:48 -0500

    calculator comments

--- Calculator.java
+++ Calculator.java
@@ -1,42 +1,42 @@
-// This is the main class for our calculator. It handles the user interface
-// and all the application's logic. It uses the other classes we created.
-// This class demonstrates Encapsulation and Polymorphism.
-//----------------------------------------------
+// This is the most important file! It's the "brain" of our app.
+// It creates the window, the buttons, the display, and handles all the logic.
+// This is where we see Encapsulation and Polymorphism in action.
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Calculator {
-    // These are private fields. They can only be accessed from within this class.
-    // This is Encapsulation: it protects the internal state of our calculator.
-    private JFrame frame;
-    private JTextField display;
-    private double currentNumber;
-    private String currentOperator;
-    private boolean startOfNewNumber;
+    // These are our "private" variables.
+    // They can ONLY be used inside this Calculator class.
+    // This is Encapsulation! It protects our data from being changed by accident.
+    private JFrame frame; // The main window of our app.
+    private JTextField display; // The screen that shows numbers.
+    private double currentNumber; // The number we're currently working with.
+    private String currentOperator; // The math symbol we're using (+, -, etc.).
+    private boolean startOfNewNumber; // Tells us if we just started typing a new number.
 
-    // This is the constructor. It runs when we create a new Calculator object.
-    // It sets up our entire user interface.
+    // This is the "constructor." It's a special method that runs as soon as we
+    // create a new Calculator object. It's where we set everything up.
     public Calculator() {
-        // Create the main window for the calculator
+        // Let's create our window.
         frame = new JFrame("Simple Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 400);
         frame.setLayout(null);
 
-        // Create the text field for the display
+        // Now we make the display screen and put it at the top.
         display = new JTextField("0");
         display.setBounds(10, 10, 265, 50);
-        display.setEditable(false);
+        display.setEditable(false); // We don't want people typing in the display.
         frame.add(display);
 
-        // Initialize our calculator's state
+        // We set our starting values.
         currentNumber = 0;
-        currentOperator = "=";
-        startOfNewNumber = true;
+        currentOperator = "="; // We start with the equals sign.
+        startOfNewNumber = true; // We're ready to type our first number.
 
-        // This is where we create the buttons and add them to the window.
-        // We will create them in a grid-like pattern.
+        // This is a list of all the button labels we want to have.
         String[] buttonLabels = {
             "7", "8", "9", "/",
             "4", "5", "6", "*",
@@ -44,6 +44,7 @@ public class Calculator {
             "0", "C", "=", "+"
         };
 
+        // We'll loop through our list of buttons to create and place them on the window.
         int x = 10;
         int y = 70;
         for (String label : buttonLabels) {
@@ -51,11 +52,12 @@ public class Calculator {
             button.setBounds(x, y, 60, 60);
             frame.add(button);
 
-            // Add an "ActionListener" to each button.
-            // This is how our program knows when a button is clicked.
+            // This is how we make our buttons do something!
+            // We're telling each button to listen for a click, and when it's clicked,
+            // it should use our special `ButtonClickListener` class.
             button.addActionListener(new ButtonClickListener());
 
-            // Move to the next position for the next button
+            // We change the x and y values to move to the next spot for the next button.
             x += 70;
             if (x > 220) {
                 x = 10;
@@ -63,45 +65,46 @@ public class Calculator {
             }
         }
 
-        // Make the window visible
+        // Finally, we tell the window to show up on the screen.
         frame.setVisible(true);
     }
 
-    // This is an inner class that handles all button clicks.
+    // This is a special class that sits inside our main Calculator class.
+    // Its only job is to handle clicks from any of our buttons.
     private class ButtonClickListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            // Get the text from the button that was clicked
+            // Get the text from the button that was just clicked (like "7" or "+").
             String command = e.getActionCommand();
 
-            // Check if the clicked button is a number
+            // We use an "if-else if-else" to figure out what kind of button it was.
             if (command.matches("[0-9]")) {
+                // If it's a number, we call our "handleNumber" method.
                 handleNumber(command);
-            }
-            // Check if it's the "C" (Clear) button
-            else if (command.equals("C")) {
+            } else if (command.equals("C")) {
+                // If it's "C", we call our "handleClear" method.
                 handleClear();
-            }
-            // Otherwise, it must be an operator
-            else {
+            } else {
+                // If it's not a number or "C", it must be an operator, so we
+                // call our "handleOperator" method.
                 handleOperator(command);
             }
         }
     }
 
-    // Method to handle when a number button is pressed
+    // This method runs when we click a number button.
     private void handleNumber(String number) {
         if (startOfNewNumber) {
-            // If it's a new number, replace the display with this number
+            // If we just cleared or pressed an operator, the display should be replaced.
             display.setText(number);
             startOfNewNumber = false;
         } else {
-            // Otherwise, add the number to the end of the display
+            // If we're still typing the same number, we just add the new digit to the end.
             display.setText(display.getText() + number);
         }
     }
 
-    // Method to handle when the clear button is pressed
+    // This method runs when we click the "C" button. It resets everything.
     private void handleClear() {
         display.setText("0");
         currentNumber = 0;
@@ -109,31 +112,32 @@ public class Calculator {
         startOfNewNumber = true;
     }
 
-    // Method to handle when an operator button is pressed
+    // This method runs when we click an operator button (+, -, etc.).
     private void handleOperator(String operator) {
-        // Get the number currently in the display
+        // First, we grab the number that's currently on the display.
         double displayNumber = Double.parseDouble(display.getText());
 
-        // This is where Polymorphism happens!
-        // We create an Operation object based on the 'currentOperator'
-        // and call its 'perform' method.
+        // This is the cool part, where Polymorphism comes in!
+        // We call our special `getOperation` method and give it the last operator we pressed.
+        // It's going to give us back the correct Operation object (Addition, Subtraction, etc.).
         Operation op = getOperation(currentOperator);
-        // The 'perform' method will call the correct function (addition, subtraction, etc.)
+        
+        // Then we use that object to perform the calculation!
+        // We don't have to know if it's an Addition or Subtraction object;
+        // we just know it has a `perform` method we can use.
         currentNumber = op.perform(currentNumber, displayNumber);
         
-        // Update the display with the new result
+        // We update the display with the new result.
         display.setText(String.valueOf(currentNumber));
 
-        // Set the new operator
+        // And finally, we get ready for the next operation.
         currentOperator = operator;
         startOfNewNumber = true;
     }
     
-    // This helper method returns the correct operation object based on the operator string.
-    // It's another great example of Polymorphism in action.
+    // This is a helper method that does the polymorphic magic.
+    // Based on the operator symbol, it gives us back the right Operation object.
     private Operation getOperation(String operator) {
-        // This 'switch' statement checks the operator string and
-        // returns the correct class object.
         switch (operator) {
             case "+":
                 return new Addition();
@@ -143,10 +147,12 @@ public class Calculator {
                 return new Multiplication();
             case "/":
                 return new Division();
-            // The default case handles the first number entered or the equals button.
+            // This is the "default" case. It runs if none of the others match.
+            // We use this for when the calculator first starts, or after the "=" button is pressed.
             default:
-                // We're returning a simple operation that just returns the second number.
-                // This is a neat trick to handle the first number correctly.
+                // This is a special way to make a temporary Operation object on the fly.
+                // It's like a dummy operation that just returns the second number,
+                // which is exactly what we want when we're entering the first number.
                 return new Operation() {
                     @Override
                     public double perform(double num1, double num2) {
@@ -156,9 +162,9 @@ public class Calculator {
         }
     }
     
-    // The main method is the starting point of our program.
-    // It simply creates a new Calculator object to run the app.
+    // This is the very first thing that runs when we start the program.
     public static void main(String[] args) {
+        // This is a special command that makes sure our app window is created safely.
         SwingUtilities.invokeLater(Calculator::new);
     }
 }
