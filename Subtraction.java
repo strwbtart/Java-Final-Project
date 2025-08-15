eb19447763c86fc6d2c10889387305e1c82398db
strwbtart <emmapach05@gmail.com>
2025-08-15 18:54:17 -0500

    subtraction comments

--- Subtraction.java
+++ Subtraction.java
@@ -1,8 +1,9 @@
-// This class also extends the Operation class, providing the logic for subtraction.
-//----------------------------------------------
+// Same thing here, but for subtraction.
+// We're extending the Operation blueprint and making our own version of the "perform" method.
+
 public class Subtraction extends Operation {
-    // This is our concrete implementation of 'perform' for subtraction.
-    
+    // This method does what it says: it subtracts the second number from the first.
+    @Override
     public double perform(double num1, double num2) {
         return num1 - num2;
     }
